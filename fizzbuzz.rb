834a71c7d9e84c8ec7566b916bff0175ccbd93e6
John Zapata <john.zapata@thezapata.com>
2018-11-21 12:51:53 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
+
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif int % 3 == 0
+    "Fizz"
+  else
+    puts "Number is not divisible by 3 or 5"
+  end
+
+end
